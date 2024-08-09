38923d3692f7947884905647975ec0882002b56c
Alexs <ale.osantos@hotmail.com>
2024-08-09 21:32:31 +0100

    feat: add header component, search and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,38 @@
 "use client"
 
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
+import { Input } from "./_components/ui/input"
 import { Button } from "./_components/ui/button"
+import Image from "next/image"
 
 const Home = () => {
-  return <Button>Test</Button>
+  return (
+    <div>
+      {/* header */}
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Alex!</h2>
+        <p>Sexta-feira, 9 de Agosto</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/banner-01.png"
+            alt="Agende nos melhores com Barber Shop"
+            fill
+            className="object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
